64deb178b39d9a3524826af1268b5ebdd9c66219
toniwilson <willson.allexandriia09@gmail.com>
2024-09-16 11:24:14 -0400

    First upload

---
+++ P1LAB1_WilsonToni.py
@@ -0,0 +1,13 @@
+# Wilson Toni-Gay
+# 09/16/2025
+# P1LAB1
+# Get input from user and display output
+
+# Get first name from the user
+first_name = input("Enter your first name: ")
+
+# Get last name from the user
+last_name = input("Enter your last name: ")
+
+# Display info to the user
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
