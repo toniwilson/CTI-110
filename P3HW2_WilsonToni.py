772bea1226ffe13251606fc02b2f93e9b23c41fc
toniwilson <willson.allexandriia09@gmail.com>
2024-10-23 11:09:19 -0400

    Add files via upload

---
+++ P3HW2_WilsonToni.py
@@ -0,0 +1,28 @@
+# Wilson Toni
+# 10-23-2024
+# P3HW2
+# Calculate reg and ot pay for an employee
+
+'''
+input : Get name from user as string
+input : Get number of hours worked as an integer
+input : Get employee's regular pay rate as a float
+
+output a dashed line and the employee name
+
+if employee has overtime (hours worked > 40)
+Calculate OT hours (hours worked - 40)
+Calculate OT pay rate (reg pay rate * 1.5)
+Determine reg hours worked - has to be 40
+Calculate pay for reg hours (40 * reg pay rate)
+Calculate OT pay (OT hours * OT pay rate)
+Calculate gross pay (reg pay + overtime pay)
+
+else (has no overtime)
+hours worked is the original value from the beginning
+pay rate is the input from the beginning
+OT hours is 0
+OT pay is 0
+Calculate pay for reg hours (hours worked * 40)
+Calculate gross pay (reg pay + 0)
+'''
