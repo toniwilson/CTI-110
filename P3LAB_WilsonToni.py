a06063c8026d374f2112339e39cedc7ccd592937
toniwilson <willson.allexandriia09@gmail.com>
2024-10-27 20:39:51 -0400

    Add files via upload

---
+++ P3LAB_WilsonToni.py
@@ -0,0 +1,60 @@
+# Wilson Toni
+# 10-27-2024
+# P3LAB
+# Display the most efficient number of dollars, quarters, nickels,dimes and pennies
+
+# Get value from user
+
+change = float(input("Enter an amount of money: $"))
+print (f"Change Amount: {change}")
+
+# Converting the calue to an integer
+
+change = round(change *  100)
+print(f"Change Amount: {change}")
+
+# Determine how many dollars are needed
+
+num_dollars = change // 100
+change = change - (num_dollars * 100)
+
+num_quarters = change // 25
+change = change - (num_quarters * 25)
+
+num_dimes = change // 10
+change = change - (num_dimes * 10)
+
+num_nickels = change // 5
+change = change - (num_nickels * 5)
+
+num_pennies = change
+
+if num_dollars > 0:
+    if num_dollars == 1:
+        print(f"{num_dollars} Dollar")
+    else:
+        print(f"{num_dollars} Dollars")
+
+if num_quarters > 0:
+    if num_quarters == 1:
+        print(f"{num_quarters} Quarter")
+    else:
+        print(f"{num_quarters} Quarters")
+
+if num_dimes > 0:
+    if num_dimes == 1:
+        print(f"{num_dimes} Dime")
+    else:
+        print(f"{num_dimes} Dimes")
+
+if num_nickels > 0:
+    if num_nickles == 1:
+        print(f"{num_nickels} Nickel")
+    else:
+        print(f"{num_nickels} Nickels")
+
+if num_pennies > 0:
+    if num_pennies == 1:
+        print(f"{num_pennies} Penny")
+    else:
+        print(f"{num_pennies} Pennies")
