58d8e6ef33aeab5b74611d4a29c1d5cfb647445d
toniwilson <willson.allexandriia09@gmail.com>
2024-11-11 16:35:08 +0000

    added value returning functions

--- function_examples.py
+++ function_examples.py
@@ -10,6 +10,18 @@ def add (num1, num2, num3):
     result = num1 + num2 + num3
     print(result)
 
+def returnNum():
+    userInput = input("Give me a big number: ")
+    while not userInput.isnumeric():
+        print("Value must be a number")
+        userInput = input("Give me a big number: ")
+    return int(userInput)
+
+def getName(lastName):
+    name = input("Enter your first name: ")
+    fullname = "*****************" + name + "*****************" + lastName
+    return fullname
+
 # Define the main function - all your logic goes here
 def main():
     # Get input from user
@@ -22,6 +34,13 @@ def main():
 
     # Call the add function
     add(first_num, second_num, third_num)
+
+    # Call the value returning function
+    bigNum = returnNum()
+    print(bigNum * 5)
+
+    # Call the last name function
+    print(getName("Wilson"))
 
 # Call the main function
 if __name__ == "__main__":
